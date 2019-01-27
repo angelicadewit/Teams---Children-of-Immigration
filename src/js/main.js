59c249a70fb8670f8d11fd451503131fb5752a7b
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-01-27 13:37:15 -0500

    changed some css

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,29 @@ var rellax = new Rellax('.rellax', {
     horizontal: false
 });
 
+// let xTo = .2*document.querySelector('.first-part').offsetWidth - document.querySelector(".airplane-stuff").offsetWidth;
+
 var waypoint = new Waypoint({
     element: document.getElementById('first-trigger'),
     handler: function(direction) {
-        console.log(`TRIGGERED`)
-        TweenMax.to(".airplane-stuff", 5, {x:1000});
-        TweenMax.to(".airplane-dust", 2, {opacity: 1, delay: 1.5});
-        TweenMax.to(".michigan", 1, {opacity: 1, delay: 4});
+            console.log(`TRIGGERED`)
+            TweenMax.to(".airplane-stuff", 5, {x:1000});
+            TweenMax.to(".airplane-dust", 2, {opacity: 1, delay: 1.5});
+            TweenMax.to(".michigan", 1, {opacity: 1, delay: 4});
     }
 })
+
+var waypoint = new Waypoint({
+    element: document.getElementById('first-trigger-hide'),
+    handler: function(direction) {
+
+        if (direction === "up"){
+            console.log(`QQ`)
+            TweenMax.to(".airplane-stuff", 5, {x:-1000});
+            TweenMax.to(".airplane-dust", 2, {opacity: 0});
+            TweenMax.to(".michigan", 1, {opacity: 0});
+        }
+    }
+})
+
+
