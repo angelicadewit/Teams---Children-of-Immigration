cebe5e3ac9dbb20084a2a1821299ee1bbe40382d
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-01-30 13:11:42 -0500

    added more images and positioning stuff

--- src/js/main.js
+++ src/js/main.js
@@ -9,23 +9,18 @@ var rellax = new Rellax('.rellax', {
 
 // let xTo = .2*document.querySelector('.first-part').offsetWidth - document.querySelector(".airplane-stuff").offsetWidth;
 
-var waypoint = new Waypoint({
+new Waypoint({
     element: document.getElementById('first-trigger'),
+    offset: "50%",
     handler: function(direction) {
+        if (direction === "down") {
             console.log(`TRIGGERED`)
-            TweenMax.to(".airplane-stuff", 5, {x:1000});
+            TweenMax.to(".airplane", 5, {rotation: "720deg", x:1000});
             TweenMax.to(".airplane-dust", 2, {opacity: 1, delay: 1.5});
             TweenMax.to(".michigan", 1, {opacity: 1, delay: 4});
-    }
-})
-
-var waypoint = new Waypoint({
-    element: document.getElementById('first-trigger-hide'),
-    handler: function(direction) {
-
-        if (direction === "up"){
+        } else if (direction === "up"){
             console.log(`QQ`)
-            TweenMax.to(".airplane-stuff", 5, {x:-1000});
+            TweenMax.to(".airplane", 5, {x:-1000});
             TweenMax.to(".airplane-dust", 2, {opacity: 0});
             TweenMax.to(".michigan", 1, {opacity: 0});
         }
