32e3b0d4b67925b760955f5fe4a6f22f173bd1f8
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-02-01 19:48:50 -0500

    edited scene one to fit three different viewports

--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,11 @@ new Waypoint({
     handler: function(direction) {
         if (direction === "down") {
             console.log(`TRIGGERED`)
-            TweenMax.to(".airplane", 5, {rotation: "720deg", x:1000});
+            let tl = new TimelineMax();
+            tl.to(".airplane",1, {y: -170, ease: Power2.easeOut})
+            tl.to(".airplane", 4, {y: 0, ease: Power2.easeInOut})
+            TweenMax.from(".airplane", 5, {rotation: "-50deg", ease: Power1.easeInOut});
+            TweenMax.to(".airplane", 5, {x:450});
             TweenMax.to(".airplane-dust", 2, {opacity: 1, delay: 1.5});
             TweenMax.to(".michigan", 1, {opacity: 1, delay: 4});
         } else if (direction === "up"){
